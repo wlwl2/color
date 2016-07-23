c6db9e23fae67c6327792d91ee05d6d664f7876f
William Lim <manvinedi@gmail.com>
2016-07-24 07:17:47 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function componentToHex(c) {
+
+/*function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
@@ -7,7 +8,7 @@ function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-//document.write( rgbToHex(0, 51, 255) ); // #0033ff
+document.write( rgbToHex(0, 51, 255) ); // #0033ff*/
 
 var inputColor = document.querySelector(".input-color");
 var colorPicker = document.querySelector(".color-picker");
@@ -16,4 +17,12 @@ inputColor.value = colorPicker.value;
 
 inputColor.addEventListener("change", function() {
     colorPicker.value = inputColor.value;
+
+    if (inputColor.value.search(/#fff/) !== -1) {
+      console.log("can white");
+    }
+
+    if (inputColor.value.search(/#fff/) !== -1) {
+      console.log("can white");
+    }
 })
