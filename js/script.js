366ec867b81e57d7731cd2e9863b70eee0a1b575
William Lim <manvinedi@gmail.com>
2016-07-24 12:06:07 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-
-/*function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-document.write( rgbToHex(0, 51, 255) ); // #0033ff*/
-
 var inputColor = document.querySelector(".input-color");
 var colorPicker = document.querySelector(".color-picker");
 
 inputColor.value = colorPicker.value;
 
+function rgbToHex(r, g, b) {
+  function componentToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+  }
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 inputColor.addEventListener("change", function() {
-    colorPicker.value = inputColor.value;
+  // if 3 digit hex entered, make into 6 digit hex
+  if (inputColor.value.search(/^#...$/) !== -1) {
+    var firstCharacter = inputColor.value.charAt(1);
+    var secondCharacter = inputColor.value.charAt(2);
+    var thirdCharacter = inputColor.value.charAt(3);
+    inputColor.value = "#" + firstCharacter + firstCharacter + secondCharacter +
+    secondCharacter + thirdCharacter + thirdCharacter;
+  }
 
-    if (inputColor.value.search(/#fff/) !== -1) {
-      console.log("can white");
-    }
-
-    if (inputColor.value.search(/#fff/) !== -1) {
-      console.log("can white");
-    }
+  // format example: rgb(111, 50, 121)
+  if (inputColor.value.search(/^[r][g][b][(]/) !== -1 &&
+      inputColor.value.match(/\d,/g).length === 2 &&
+      inputColor.value.search(/[)]/) !== -1)
+  {
+    document.querySelector(".rgb-output").innerHTML = inputColor.value;
+    var regExp = /\(([^)]+)\)/;
+    var rgbList = regExp.exec(inputColor.value)[1].split(",");
+    var red = Number(rgbList[0]);
+    var green = Number(rgbList[1]);
+    var blue = Number(rgbList[2]);
+    inputColor.value = rgbToHex(red, green, blue);
+    document.querySelector(".hex-output").innerHTML = rgbToHex(red, green, blue);
+  }
+  colorPicker.value = inputColor.value;
 })
