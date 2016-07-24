97c0ceac1e21df351aa0561d0182284a8d0a76b6
William Lim <manvinedi@gmail.com>
2016-07-24 14:15:19 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 color-picker
 
 http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+
+Back to my Github Page:
+https://wlwl2.github.io/
--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 var inputColor = document.querySelector(".input-color");
 var colorPicker = document.querySelector(".color-picker");
+var hexOutput = document.querySelector(".hex-output");
+var rgbOutput = document.querySelector(".rgb-output");
 
 inputColor.value = colorPicker.value;
 
+//RGB to hex conversion and add any required zero padding
 function rgbToHex(r, g, b) {
   function componentToHex(c) {
       var hex = c.toString(16);
       return hex.length == 1 ? "0" + hex : hex;
   }
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
+
+/*function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}*/
+
+// Here's a version of hexToRgb() that also parses a shorthand hex triplet such as "#03F"
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }
 
 inputColor.addEventListener("change", function() {
@@ -22,18 +45,27 @@ inputColor.addEventListener("change", function() {
   }
 
   // format example: rgb(111, 50, 121)
-  if (inputColor.value.search(/^[r][g][b][(]/) !== -1 &&
+  if (inputColor.value.search(/[r][g][b][(]/) !== -1 &&
       inputColor.value.match(/\d,/g).length === 2 &&
       inputColor.value.search(/[)]/) !== -1)
   {
-    document.querySelector(".rgb-output").innerHTML = inputColor.value;
+    rgbOutput.innerHTML = inputColor.value;
     var regExp = /\(([^)]+)\)/;
     var rgbList = regExp.exec(inputColor.value)[1].split(",");
     var red = Number(rgbList[0]);
     var green = Number(rgbList[1]);
     var blue = Number(rgbList[2]);
     inputColor.value = rgbToHex(red, green, blue);
-    document.querySelector(".hex-output").innerHTML = rgbToHex(red, green, blue);
+    hexOutput.innerHTML = rgbToHex(red, green, blue);
   }
   colorPicker.value = inputColor.value;
-})
+});
+
+colorPicker.addEventListener("change", function(){
+  colorPicker.value = colorPicker.value;
+  inputColor.value = colorPicker.value;
+  hexOutput.innerHTML = inputColor.value;
+  rgbOutput.innerHTML = "rgb("+ hexToRgb(inputColor.value).r.toString() + ", " +
+  hexToRgb(inputColor.value).g.toString() + ", " +
+  hexToRgb(inputColor.value).b.toString() + ")";
+});
