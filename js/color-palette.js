22ed3193b825ad8af4e96f746143b7166e2c72ae
William Lim <manvinedi@gmail.com>
2017-02-18 19:24:46 +0800

    update

--- js/color-palette.js
+++ js/color-palette.js
@@ -37,28 +37,68 @@ function hexToRgb(hex) {
 
 var colorList = [
   {
-    "color": "red",
-    "backgroundColor" : "#b91b1b"
+    "colorName": "base03",
+    "backgroundColor" : "#002b36"
   },
   {
-    "color": "green",
-    "backgroundColor" : "#1e6712"
+    "colorName": "base02",
+    "backgroundColor" : "#073642"
   },
   {
-    "color": "blue",
-    "backgroundColor" : "#0b2732"
+    "colorName": "base01",
+    "backgroundColor" : "#586e75"
   },
   {
-    "color": "yellow",
-    "backgroundColor" : "#cfc32c"
+    "colorName": "base00",
+    "backgroundColor" : "#657b83"
   },
   {
-    "color": "purple",
-    "backgroundColor" : "#8965ad"
+    "colorName": "base0",
+    "backgroundColor" : "#839496"
   },
   {
-    "color": "orange",
-    "backgroundColor" : "#e8af4d"
+    "colorName": "base1",
+    "backgroundColor" : "#93a1a1"
+  },
+  {
+    "colorName": "base2",
+    "backgroundColor" : "#eee8d5"
+  },
+  {
+    "colorName": "base3",
+    "backgroundColor" : "#fdf6e3"
+  },
+  {
+    "colorName": "yellow",
+    "backgroundColor" : "#b58900"
+  },
+  {
+    "colorName": "orange",
+    "backgroundColor" : "#cb4b16"
+  },
+  {
+    "colorName": "red",
+    "backgroundColor" : "#dc322f"
+  },
+  {
+    "colorName": "magenta",
+    "backgroundColor" : "#d33682"
+  },
+  {
+    "colorName": "violet",
+    "backgroundColor" : "#6c71c4"
+  },
+  {
+    "colorName": "blue",
+    "backgroundColor" : "#268bd2"
+  },
+  {
+    "colorName": "cyan",
+    "backgroundColor" : "#2aa198"
+  },
+  {
+    "colorName": "green",
+    "backgroundColor" : "#859900"
   }
 ]
 
