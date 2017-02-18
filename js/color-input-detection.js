818c12e852abde295a3600fb8191ec9048814b8a
William Lim <manvinedi@gmail.com>
2017-02-19 01:08:55 +0800

    fixed ie bugs

--- js/color-input-detection.js
+++ js/color-input-detection.js
@@ -1,14 +1,8 @@
 (function colorInputDetection(){
   var colorPicker = document.querySelectorAll(".color-picker")[0];
-  if (colorPicker) {
-    if (colorPicker.value.length !== 7) {
-      document.querySelector('.color-picker-main-container').setAttribute("style", "display:none;");
-      console.log("your browser doesn't support this color picker");
-      document.write("your browser doesn't support this color picker");
-    }
-  } else {
+  if (colorPicker.value.length !== 7) {
     document.querySelector('.color-picker-main-container').setAttribute("style", "display:none;");
-    console.log("your browser doesn't support this color picker");
-    document.write("your browser doesn't support this color picker");
+    console.log("Your browser doesn't support this color picker. Use the latest version of Google Chrome, Firefox, or try another browser.");
+    document.write("Your browser doesn't support this color picker. Use the latest version of Google Chrome, Firefox, or try another browser.");
   }
 })();
