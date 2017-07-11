dad3c1c9aad7e007eb8b54a852e6116a20c023b7
William Lim <manvinedi@gmail.com>
2017-07-11 16:14:18 +0800

    relaxed browser support

--- js/color-input-detection.js
+++ js/color-input-detection.js
@@ -1,8 +1,8 @@
-(function colorInputDetection(){
-  var colorPicker = document.querySelectorAll(".color-picker")[0];
-  if (colorPicker.value.length !== 7) {
-    document.querySelector('.color-picker-main-container').setAttribute("style", "display:none;");
-    console.log("Your browser doesn't support this color picker. Use the latest version of Google Chrome, Firefox, or try another browser.");
-    document.write("Your browser doesn't support this color picker. Use the latest version of Google Chrome, Firefox, or try another browser.");
-  }
-})();
+// (function colorInputDetection(){
+//   var colorPicker = document.querySelectorAll(".color-picker")[0]
+//   if (colorPicker.value.length !== 7) {
+//     document.querySelector('.color-picker-main-container').setAttribute("style", "display:none;")
+//     console.log("Your browser doesn't support this color picker. Use the latest version of Google Chrome, Firefox, or try another browser.")
+//     document.write("Your browser doesn't support this color picker. Use the latest version of Google Chrome, Firefox, or try another browser.")
+//   }
+// })()
