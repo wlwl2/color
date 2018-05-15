c74884f3e18917b245616c05ed68354b72116c2c
William Lim <manvinedi@gmail.com>
2018-05-15 16:22:14 +0800

    scrolling

--- js/script.js
+++ js/script.js
@@ -153,7 +153,7 @@
   }
 })();
 
-var content = document.querySelector('.body-container');
+var content = document.body;
 content.addEventListener('touchstart', function(event) {
     this.allowUp = (this.scrollTop > 0);
     this.allowDown = (this.scrollTop < this.scrollHeight - this.clientHeight);
