23143257ff1d89a7877d97aac985bff8095992ca
William Lim <manvinedi@gmail.com>
2020-03-28 03:11:48 +0800

    updated red colors

--- palettes/x11/red.js
+++ palettes/x11/red.js
@@ -1,10 +1,10 @@
 Red colors
-LightSalmon	FF A0 7A	255 160 122
-Salmon	FA 80 72	250 128 114
-DarkSalmon	E9 96 7A	233 150 122
-LightCoral	F0 80 80	240 128 128
-IndianRed	CD 5C 5C	205  92  92
-Crimson	DC 14 3C	220  20  60
-FireBrick	B2 22 22	178  34  34
-DarkRed	8B 00 00	139   0   0
-Red	FF 00 00	255   0   0
+LightSalmon	#FFA07A	255 160 122
+Salmon	#FA8072	250 128 114
+DarkSalmon	#E9967A	233 150 122
+LightCoral	#F08080	240 128 128
+IndianRed	#CD5C5C	205 92 92
+Crimson	#DC143C	220 20 60
+FireBrick	#B22222	178 34 34
+DarkRed	#8B0000	139 0 0
+Red	#FF0000	255 0 0
