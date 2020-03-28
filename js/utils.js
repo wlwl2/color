7c6c98d42d077c67374d31f0ff7e928c26c96bde
William Lim <manvinedi@gmail.com>
2020-03-29 03:32:10 +0800

    added more color util functions

--- js/utils.js
+++ js/utils.js
@@ -63,6 +63,68 @@ function hexToRgb (hex) {
   } : null
 }
 
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ * (https://gist.github.com/mjackson/5311256)
+ * @param {number} r - The red color value.
+ * @param {number} g - The green color value.
+ * @param {number} b - The blue color value.
+ * @returns {(number|Array)} - The HSV representation.
+ */
+function rgbToHsv (r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, v = max;
+
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
+
+  if (max == min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, v ];
+}
+
+/**
+ * I: An array of three elements hue (h) ∈ [0, 360], and saturation (s) and value (v) which are ∈ [0, 1]
+ * O: An array of red (r), green (g), blue (b), all ∈ [0, 255]
+ * Derived from https://en.wikipedia.org/wiki/HSL_and_HSV
+ * This stackexchange was the clearest derivation I found to reimplement https://cs.stackexchange.com/questions/64549/convert-hsv-to-rgb-colors
+ */
+function hsvToRgb (h, s, v) {
+  hprime = h / 60;
+  const c = v * s;
+  const x = c * (1 - Math.abs(hprime % 2 - 1)); 
+  const m = v - c;
+  let r, g, b;
+  if (!hprime) {r = 0; g = 0; b = 0; }
+  if (hprime >= 0 && hprime < 1) { r = c; g = x; b = 0}
+  if (hprime >= 1 && hprime < 2) { r = x; g = c; b = 0}
+  if (hprime >= 2 && hprime < 3) { r = 0; g = c; b = x}
+  if (hprime >= 3 && hprime < 4) { r = 0; g = x; b = c}
+  if (hprime >= 4 && hprime < 5) { r = x; g = 0; b = c}
+  if (hprime >= 5 && hprime < 6) { r = c; g = 0; b = x}
+  
+  r = Math.round( (r + m)* 255);
+  g = Math.round( (g + m)* 255);
+  b = Math.round( (b + m)* 255);
+
+  return [r, g, b]
+}
+
 // /**
 //  * Just an alternative version of rgbToHex().
 //  * Note: Expects integer values for r, g and b, so you'll need to do 
